57545490cc0c872779754b23d0fa6f24c208a431
Brian Lee <leebri2n@vt.edu>
2025-03-27 09:59:07 -0400

    Reduce requirements

--- codex/__init__.py
+++ codex/__init__.py
@@ -1,3 +1,2 @@
 from .modules import combinatorial, output, binning
-from .utils import codex_metrics, codex_plotting, coverage_mapping, dataset_handler, input_handler, sie_yolo_ml
-from .utils import sie_analysis
+from .utils import codex_metrics, codex_plotting, coverage_mapping, dataset_handler, input_handler
